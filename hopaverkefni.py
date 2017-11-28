dde522e82705a8b43167c3d2fd9863bf9d34d8c9
JHT1609 <johannesto245@nemi.tskoli.is>
2017-11-28 09:26:20 +0000

    valmynd og uppsettning 1

---
+++ hopaverkefni.py
@@ -0,0 +1,17 @@
+#Jóhannes Helgi Tómasson & Stefán Elís Beck
+
+
+#Username & password
+Users = {"kalli_kool":"dr4gon","Fannar_flotti":"password","oni0nboie_66":"rossiyagay"}
+
+print("Viltu skrá nýjan notanda eða ertu nú þegar skráður inn á kerfið?\n######################\n#1.Skrá Nýjan notanda#\n#2.Skrá inn          #\n######################\n")
+
+SL=input()
+if SL=="2":
+    print("##############\n# LOGIN AREA #\n##############")
+    UN=input("Sláðu inn notandanafn: ")
+    PW=input("Sláðu inn Leynikóðan:  ")
+elif SL =="1":
+    pass
+else:
+    pass
